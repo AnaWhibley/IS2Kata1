97689e12fdbd178016a1de8ac248b039f8d94f58
Ana <ana@MacBook-Pro-de-Ana.local>
2019-09-20 20:35:03 +0100

    V3 Kata1 completed, improved version using LocalDate for birthday

--- src/is2kata1/Main.java
+++ src/is2kata1/Main.java
@@ -1,14 +1,16 @@
 package is2kata1;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        Calendar date = GregorianCalendar.getInstance();
-        date.set(1997,3,10);
-
-        Person ana = new Person("Ana", date);
+        Person ana = new Person("Ana", LocalDate.of(1997,3,10));
         System.out.println(ana.getName() + " is " + ana.getAge() + " years old");
+
+        Person jose = new Person("Jose", LocalDate.of(1963,9,23));
+        System.out.println(jose.getName() + " is " + jose.getAge() + " years old");
+
+        Person pedro = new Person("Pedro", LocalDate.of(1991,7,1));
+        System.out.println(pedro.getName() + " is " + pedro.getAge() + " years old");
     }
 }
--- src/is2kata1/Person.java
+++ src/is2kata1/Person.java
@@ -1,18 +1,13 @@
 package is2kata1;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+import java.time.LocalDate;
+import java.time.Period;
 
 public class Person {
     private final String name;
-    private final Calendar birthday;
-    private final double DAYS_PER_YEAR = 365.25;
-    private final int HOURS_PER_DAY = 24;
-    private final int MINUTES_PER_HOUR = 60;
-    private final int SECONDS_PER_MINUTES = 60;
-    private final int MILISECONDS_PER_SECONDS = 1000;
+    private final LocalDate birthday;
 
-    public Person(String name, Calendar birthday) {
+    public Person(String name, LocalDate birthday) {
         this.name = name;
         this.birthday = birthday;
     }
@@ -21,24 +16,15 @@ public class Person {
         return name;
     }
 
-    public Calendar getBirthday() {
+    public LocalDate getBirthday() {
         return birthday;
     }
 
     public int getAge() {
-        return calculateAge(getBirthday());
+        return calculateAge();
     }
 
-    private int calculateAge(Calendar birthday) {
-        Calendar today = GregorianCalendar.getInstance();
-        return (int) (millisecondsToYears(today.getTimeInMillis())- millisecondsToYears(birthday.getTimeInMillis()));
-    }
-
-    private long calculateMillisecondsPerYear() {
-        return (long) DAYS_PER_YEAR*HOURS_PER_DAY*MINUTES_PER_HOUR*SECONDS_PER_MINUTES*MILISECONDS_PER_SECONDS;
-    }
-
-    private long millisecondsToYears(long milliseconds) {
-        return milliseconds / calculateMillisecondsPerYear();
+    private int calculateAge() {
+        return Period.between(getBirthday(),LocalDate.now()).getYears();
     }
 }
