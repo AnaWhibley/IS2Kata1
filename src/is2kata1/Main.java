622d23b55d19529fea686d9bbcf7a90dc420400a
Ana <ana@MacBook-Pro-de-Ana.local>
2019-09-20 15:25:53 +0100

    V2 Kata1 finished. Added birthday as Calendar and the corresponding modifications

--- src/is2kata1/Main.java
+++ src/is2kata1/Main.java
@@ -1,12 +1,14 @@
 package is2kata1;
 
-import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Main {
     public static void main(String[] args) {
-        Person ana = new Person("Ana", new Date(97,2,10));  // 10 de marzo del 97
+        Calendar date = GregorianCalendar.getInstance();
+        date.set(1997,3,10);
+
+        Person ana = new Person("Ana", date);
         System.out.println(ana.getName() + " is " + ana.getAge() + " years old");
-
-
     }
 }
--- src/is2kata1/Person.java
+++ src/is2kata1/Person.java
@@ -1,17 +1,18 @@
 package is2kata1;
 
-import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Person {
     private final String name;
-    private final Date birthday;
+    private final Calendar birthday;
     private final double DAYS_PER_YEAR = 365.25;
     private final int HOURS_PER_DAY = 24;
     private final int MINUTES_PER_HOUR = 60;
     private final int SECONDS_PER_MINUTES = 60;
     private final int MILISECONDS_PER_SECONDS = 1000;
 
-    public Person(String name, Date birthday) {
+    public Person(String name, Calendar birthday) {
         this.name = name;
         this.birthday = birthday;
     }
@@ -20,7 +21,7 @@ public class Person {
         return name;
     }
 
-    public Date getBirthday() {
+    public Calendar getBirthday() {
         return birthday;
     }
 
@@ -28,12 +29,16 @@ public class Person {
         return calculateAge(getBirthday());
     }
 
-    private int calculateAge(Date birthday) {
-        return (int) ((new Date().getTime() - birthday.getTime()) / calculateMiliseconds());
+    private int calculateAge(Calendar birthday) {
+        Calendar today = GregorianCalendar.getInstance();
+        return (int) (millisecondsToYears(today.getTimeInMillis())- millisecondsToYears(birthday.getTimeInMillis()));
     }
 
-    private double calculateMiliseconds() {
-        return DAYS_PER_YEAR*HOURS_PER_DAY*MINUTES_PER_HOUR*SECONDS_PER_MINUTES*MILISECONDS_PER_SECONDS;
+    private long calculateMillisecondsPerYear() {
+        return (long) DAYS_PER_YEAR*HOURS_PER_DAY*MINUTES_PER_HOUR*SECONDS_PER_MINUTES*MILISECONDS_PER_SECONDS;
     }
 
+    private long millisecondsToYears(long milliseconds) {
+        return milliseconds / calculateMillisecondsPerYear();
+    }
 }
